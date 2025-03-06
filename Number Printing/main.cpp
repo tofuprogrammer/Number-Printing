bc647ab83b9cac66a052aaf4cb54a44be6ecc3e2
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-03-06 11:22:41 +0000

    Created initial loop for user input
    Asking the user for a number, checking if the input is valid, and handling errors has been implemented.

--- Number Printing/main.cpp
+++ Number Printing/main.cpp
@@ -1,14 +1,22 @@
-//
-//  main.cpp
-//  Number Printing
-//
-//  Created by Sam Hart on 06/03/2025.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+    int userNumber = 0;
+    bool userNumberValid = false;
+    do {
+        std::cout << "Enter a number between 1 and 10: ";
+        std::cin >> userNumber;
+        if (!std::cin || userNumber < 1 || userNumber > 10) {
+            std::cout << "Invalid input\n";
+            if (!std::cin) {
+                std::cin.clear();
+                std::cin
+                    .ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
+            continue;
+        }
+        else {
+            userNumberValid = true;
+        }
+    } while (userNumberValid == false);
+    }
